95e6822c7bf871e4e2a7894bba08ecb4a2299f3e
Emanuel Palm <emanuel.palm@ltu.se>
2020-02-19 10:44:45 +0100

    Improve HttpPattern and write documentation

--- src/main/java/eu/arrowhead/kalix/http/HttpPattern.java
+++ src/main/java/eu/arrowhead/kalix/http/HttpPattern.java
@@ -2,6 +2,38 @@ package eu.arrowhead.kalix.http;
 
 import java.util.*;
 
+/**
+ * A pattern that matches a well-defined set of paths.
+ * <p>
+ * At its simplest, a pattern is a regular URI path, as defined by RFC 3986,
+ * Section 3.3. Such a path starts with a forward slash ({@code /}) and then
+ * continues with zero or more segments separated by for forward slashes. An
+ * optional forward slash may be located at the end. While it is recommended
+ * for segments to contain only the alphanumeric ASCII characters and hyphens
+ * to maximize compatibility with various HTTP libraries and frameworks, RFC
+ * 3986 explicitly allows the following characters to be used, except for
+ * percent encodings:
+ * <pre>
+ * A–Z a–z 0–9 - . _ ~ ! $ & ' ( ) * + , ; = : @
+ * </pre>
+ * While all these segment characters are allowed by this pattern
+ * implementation, percent encodings are not (e.g. {@code %20} as a
+ * representation for ASCII space).
+ * <p>
+ * It is frequently useful to allow certain pattern segments to match any
+ * segment in a given path. For this reason, pattern segments may be qualified
+ * as <i>path parameters</i> by adding a hash ({@code #}) at the beginning of
+ * the segment (e.g. {@code /some/#parameter} or
+ * {@code /some/other/#parameter/path}). When a path is successfully matched
+ * against a pattern, any path parameter segments are collected from the path
+ * into a list.
+ * <p>
+ * A pattern may optionally end with a right angle bracket ({@code >}) to
+ * denote that the pattern is to be considered a prefix. Prefix patterns match
+ * all paths with matching segments up until the right angle bracket.
+ *
+ * @see <a href="https://tools.ietf.org/html/rfc3986#section-3.3">RFC 3986, Section 3.3</a>
+ */
 public class HttpPattern {
     private final String pattern;
     private final int nParameters;
@@ -18,7 +50,27 @@ public class HttpPattern {
      */
     public static final HttpPattern ROOT = new HttpPattern("/", 0, false);
 
-    public boolean apply(final String path, final List<String> parameters) {
+    /**
+     * Matches this pattern against given path, and collects any path
+     * parameters into the provided list.
+     * <p>
+     * Note that {@link List#clear()} will be called on the list if any
+     * parameter is identified by the pattern in {@code path}. Furthermore,
+     * even if this method returns {@code false}, it may have collected
+     * parameters into the provided list before returning.
+     * <p>
+     * If this pattern contains path parameters or is a prefix, any segments
+     * matching a path parameter or occurs after the prefix are <b>not</b>
+     * validated, and may, therefore, contain invalid path characters, as
+     * dictated by RFC 3986.
+     *
+     * @param path       Path to match against.
+     * @param parameters List to store any pattern path parameters in.
+     * @return {@code true} only if {@code path} matches this pattern.
+     * @see HttpPattern
+     * @see <a href="https://tools.ietf.org/html/rfc3986#section-3.3">RFC 3986, Section 3.3</a>
+     */
+    public boolean match(final String path, final List<String> parameters) {
         // p0 = start of path;    p1 = end of path.
         // q0 = start of pattern; q1 = end of pattern.
 
@@ -29,11 +81,11 @@ public class HttpPattern {
         // We have already stripped any trailing '/' from the pattern.
         final var q1 = pattern.length();
 
-        if (!isPrefix && nParameters == 0 && p1 != q1) {
-            return false;
+        if (nParameters == 0) { // Fast path for patterns without parameters.
+            return (isPrefix ? p1 > q1 : p1 == q1) && pattern.regionMatches(0, path, 0, q1);
         }
 
-        var hasFoundParameter = false;
+        int np = 0; // Number of found path parameters.
         int p0 = 0, q0 = 0;
         while (true) {
             // Find first non-identical character.
@@ -45,26 +97,45 @@ public class HttpPattern {
             final var pAtEnd = p0 == p1;
             final var qAtEnd = q0 == q1;
             if (pAtEnd && qAtEnd || isPrefix && qAtEnd) {
-                return true;
+                return np == nParameters;
             }
             if (pAtEnd || qAtEnd) {
                 return false;
             }
             // We must be at a path parameter. Collect segment from path.
-            if (!hasFoundParameter) {
-                parameters.clear();
-                hasFoundParameter = true;
-            }
-            var px = p0;
+            int px = p0;
             while (px < p1 && path.charAt(px) != '/') {
                 px += 1;
             }
+
+            if (np == 0) {
+                parameters.clear();
+            }
+            np += 1;
             parameters.add(path.substring(p0, px));
+
             p0 = px;
-            q0 += 1;
+            q0 += 1; // Skip '#'.
         }
     }
 
+    /**
+     * Produces {@link HttpPattern} from given pattern string.
+     * <p>
+     * Valid patterns must start with a forward slash and consist of zero or
+     * more segments separated by forward slashes. A segment must not contain
+     * percent encodings, but may otherwise consist of any valid path segment
+     * character, as defined by RFC 3986. A segment may optionally start with
+     * a hash ({@code #}) to make it into a path parameter. The last segment
+     * may consist only of a right angle bracket ({@code >}) to make the
+     * pattern into a so-called <i>prefix-pattern</i>.
+     *
+     * @param pattern String to parse.
+     * @return New {@link HttpPattern} or {@link #ROOT} if {@code pattern} is {@code "/"}.
+     * @throws IllegalArgumentException If pattern is invalid.
+     * @see HttpPattern
+     * @see <a href="https://tools.ietf.org/html/rfc3986#section-3.3">RFC 3986, Section 3.3</a>
+     */
     public static HttpPattern valueOf(final String pattern) {
         // q0 = start of pattern; q1 = end of pattern
         var q1 = pattern.length();
@@ -78,52 +149,57 @@ public class HttpPattern {
             q1 -= 1;
         }
 
-        final var buffer = new StringBuilder(pattern.length());
+        final var builder = new StringBuilder(pattern.length());
         var nParameters = 0;
         var isPrefix = false;
 
         outer:
         for (var q0 = 0; q0 < q1; ++q0) {
-            var cp = pattern.codePointAt(q0);
-            if (cp == '%') {
+            var c = pattern.charAt(q0);
+            if (c == '%') {
                 throw new IllegalArgumentException("Percent encodings may not be used in patterns.");
             }
-            if (cp != '/') {
-                buffer.append(requirePathCharacter(cp));
-                continue;
+            if (c != '/') {
+                if (isRFC3986PathChar(c)) {
+                    builder.append(c);
+                    continue;
+                }
+                throw new IllegalArgumentException(c == '#'
+                    ? "`#` may only occur right after a `/` to promote its segment to a path parameter."
+                    : "Invalid pattern character `" + c + "`; expected one of `0–9 A–Z a–z -._~!$&'()*+,;=:@`.");
             }
             segment:
             while (true) {
-                buffer.append('/');
+                builder.append('/');
                 if (q0 + 1 == q1) {
                     break outer;
                 }
-                cp = pattern.codePointAt(q0 + 1);
-                switch (cp) {
-                case '#':
-                    buffer.append('#');
+                c = pattern.charAt(q0 + 1);
+                switch (c) {
+                case '#': // Path parameter segment. Omit parameter name from pattern.
+                    builder.append('#');
                     nParameters += 1;
                     q0 += 2;
                     while (q0 < q1) {
-                        cp = pattern.codePointAt(q0);
-                        if (cp == '/') {
+                        c = pattern.charAt(q0);
+                        if (c == '/') {
                             continue segment;
                         }
                         q0 += 1;
                     }
                     break;
 
-                case '.':
+                case '.': // Ensure segment is not a relative path directive.
                     var isRelative = false;
                     if (q0 + 2 == q1) {
                         isRelative = true; // Pattern ends with "/."
                     }
                     else {
-                        cp = pattern.codePointAt(q0 + 2);
-                        if (cp == '.' && (q0 + 3 == q1 || pattern.codePointAt(q0 + 3) == '/')) {
+                        c = pattern.charAt(q0 + 2);
+                        if (c == '.' && (q0 + 3 == q1 || pattern.charAt(q0 + 3) == '/')) {
                             isRelative = true; // Pattern ends with "/.." or contains "/../"
                         }
-                        else if (cp == '/') {
+                        else if (c == '/') {
                             isRelative = true; // Pattern contains "/./"
                         }
                     }
@@ -131,8 +207,8 @@ public class HttpPattern {
                         throw new IllegalArgumentException("Relative paths may not be used in patterns.");
                     }
 
-                case '>':
-                    if (q0 + 2 == q1) {
+                case '>': // Makes pattern into a prefix. Ensure it is the last thing in the pattern.
+                    if (q0 + 2 == pattern.length()) {
                         isPrefix = true;
                         break outer;
                     }
@@ -143,38 +219,11 @@ public class HttpPattern {
             }
         }
 
-        return new HttpPattern(buffer.toString(), nParameters, isPrefix);
+        return new HttpPattern(builder.toString(), nParameters, isPrefix);
     }
 
-    /**
-     * Requires that given code point represents a valid URI path character, or
-     * throws an exception. The following are the valid path characters:
-     * <pre>
-     * A–Z a–z 0–9 - . _ ~ % / ! $ & ' ( ) * + , ; = : @
-     * </pre>
-     *
-     * @param c Tested code point.
-     * @return {@code c} cast to {@code byte}.
-     * @throws IllegalArgumentException If {@code c} is not a valid path
-     *                                  character.
-     * @see <a href="https://tools.ietf.org/html/rfc3986#section-3.3">RFC 3986, Section 3.3</a>
-     */
-    private static char requirePathCharacter(final int c) {
-        final var isValid = c >= 0x61 && c <= 0x7A || c >= 0x24 && c <= 0x3B ||
-            c >= 0x40 && c <= 0x5A || c == 0x5F || c == 0x7E || c == 0x3D ||
-            c == 0x21;
-        if (isValid) {
-            return Character.toChars(c)[0];
-        }
-        throw new IllegalArgumentException(c == '#'
-            ? "`#` may only occur right after a `/` to promote its segment to a path parameter."
-            : "Invalid pattern character `" + Character.toString(c) + "`; "
-            + "expected one of `0–9 A–Z a–z - . _ ~ % / ! $ & ' ( ) * + , ; = "
-            + ": @`.");
-    }
-
-    @Override
-    public String toString() {
-        return pattern;
+    private static boolean isRFC3986PathChar(final char c) {
+        return c >= 'a' && c <= 'z' || c >= '$' && c <= ';' || c >= '@' && c <= 'Z' ||
+            c == '_' || c == '~' || c == '=' || c == '!';
     }
 }
--- src/test/java/eu/arrowhead/kalix/http/TestHttpPattern.java
+++ src/test/java/eu/arrowhead/kalix/http/TestHttpPattern.java
@@ -1,6 +1,5 @@
 package eu.arrowhead.kalix.http;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -13,26 +12,18 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class TestHttpPattern {
-    @Test
-    void shouldMatchRootPattern() {
-        final var parameters = new ArrayList<String>(0);
-        assertTrue(HttpPattern.valueOf("/").apply("/", parameters));
-        assertEquals(0, parameters.size());
-    }
-
-
     @ParameterizedTest
-    @MethodSource("validPatternPathParameterSets")
+    @MethodSource("matchingPatternPathParameterSets")
     void shouldMatchPattern(final String pattern, final String path, final String[] expectedParameters) {
         final var actualParameters = new ArrayList<String>(0);
         assertTrue(
-            HttpPattern.valueOf(pattern).apply(path, actualParameters),
+            HttpPattern.valueOf(pattern).match(path, actualParameters),
             "Pattern `" + pattern + "` does not match path `" + path + "`"
         );
         assertEquals(Arrays.asList(expectedParameters), actualParameters);
     }
 
-    static Stream<Arguments> validPatternPathParameterSets() {
+    static Stream<Arguments> matchingPatternPathParameterSets() {
         return Stream.of(
             arguments("/", "/", new String[0]),
             arguments("/hello", "/hello", new String[0]),
@@ -44,7 +35,61 @@ public class TestHttpPattern {
             arguments("/#A/#B/#C/", "/x/y/z", new String[]{"x", "y", "z"}),
             arguments("/#A/#B/#C/", "/x/y/z/", new String[]{"x", "y", "z"}),
             arguments("/123/#B/@:", "/123/y/@:", new String[]{"y"}),
-            arguments("/#A/>", "/x/y/z", new String[]{"x"})
+            arguments("/#A/>", "/x/y/z", new String[]{"x"}),
+            arguments("/>", "/x/y/z", new String[0]),
+            arguments("/base/#id/thing", "/base//thing", new String[]{""}),
+            arguments("//#id/thing", "///thing", new String[]{""}),
+            arguments("///thing", "///thing", new String[0]),
+            arguments("///", "///", new String[0]),
+            arguments("///#", "///maybe", new String[]{"maybe"}),
+            arguments("///>", "///hello", new String[0])
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("mismatchingPatternPathPairs")
+    void shouldNotMatchPattern(final String pattern, final String path) {
+        final var actualParameters = new ArrayList<String>(0);
+        assertFalse(
+            HttpPattern.valueOf(pattern).match(path, actualParameters),
+            "Pattern `" + pattern + "` does match path `" + path + "`"
+        );
+    }
+
+    static Stream<Arguments> mismatchingPatternPathPairs() {
+        return Stream.of(
+            arguments("/", "/hello"),
+            arguments("/hello", "/"),
+            arguments("//#1", "/"),
+            arguments("/x/>", "/x/"),
+            arguments("/#/hello", "/#/hello"),
+            arguments("/#id/hello", "/#/hello"),
+            arguments("/#id", "/#")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidPatterns")
+    void shouldNotParsePattern(final String pattern) {
+        assertThrows(
+            IllegalArgumentException.class, () -> HttpPattern.valueOf(pattern),
+            "Expected pattern `" + pattern + "` to be invalid."
+        );
+    }
+
+    static Stream<String> invalidPatterns() {
+        return Stream.of(
+            "",
+            "hello",
+            "hello/",
+            "/>/",
+            "/hello#",
+            "/hello>",
+            "/hello,%20world!",
+            "/x/y/./z",
+            "/x/y/../z",
+            "/x/y/.",
+            "/x/y/.."
         );
     }
 }
