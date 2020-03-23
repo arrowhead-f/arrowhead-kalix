950347c9885c8872e0e5be84dec69a2579e2bdaf
Emanuel Palm <emanuelpalm@gmail.com>
2020-03-23 10:12:06 +0100

    Move kalix-example into separate repository

--- kalix-example/scripts/rm_certs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Removes all keystores, certificates and truststores potentially created by
-# `mk_certs.sh`.
-
-cd "$(dirname "$0")" || exit
-cd ..
-find cloud-* -regex ".*\.\(p12\|crt\|jks\|pub\|key\|ca\)" -exec rm -f {} \;
