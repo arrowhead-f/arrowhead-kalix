arrowhead-f/arrowhead-kalix

commit a5e687bc949bd76790c6fcd54c048fa7d125821e

Fix premature client connection close bug

kalix-base/src/main/java/se/arkalix/internal/net/http/client/NettyHttpClientConnection.java
@@ -41,6 +41,8 @@ public class NettyHttpClientConnection implements HttpClientConnection {
     private final Channel channel;
     private final Queue<FutureResponse> pendingResponseQueue = new LinkedList<>();
 
+    private boolean isClosing = false;
+
     public NettyHttpClientConnection(
         final Channel channel,
         final Certificate[] certificateChain)
@@ -62,10 +64,14 @@ public class NettyHttpClientConnection implements HttpClientConnection {
     @Override
     public Certificate[] certificateChain() {
         if (certificateChain == null) {
-            throw new NotSecureException("Connection not secured;" +
-                " no certificates are available");
+            throw new NotSecureException("Connection not secured; " +
+                "no certificates are available");
         }
         return certificateChain;
+    }
+
+    public boolean isClosing() {
+        return isClosing;
     }
 
     @Override
@@ -80,12 +86,8 @@ public class NettyHttpClientConnection implements HttpClientConnection {
 
     @Override
     public Future<HttpClientResponse> send(final HttpClientRequest request) {
-        return send(request, true);
-    }
-
-    private Future<HttpClientResponse> send(final HttpClientRequest request, final boolean keepAlive) {
         try {
-            writeRequestToChannel(request, keepAlive);
+            writeRequestToChannel(request);
         }
         catch (final Throwable throwable) {
             return Future.failure(throwable);
@@ -97,17 +99,12 @@ public class NettyHttpClientConnection implements HttpClientConnection {
 
     @Override
     public Future<HttpClientResponse> sendAndClose(final HttpClientRequest request) {
-        return send(request, false)
-            .flatMapResult(result -> {
-                if (channel.isActive()) {
-                    return close().mapResult(ignored -> result);
-                }
-                return Future.of(result);
-            });
+        isClosing = true;
+        return send(request);
     }
 
     @SuppressWarnings("unchecked")
-    private void writeRequestToChannel(final HttpClientRequest request, final boolean keepAlive) throws DtoWriteException, IOException {
+    private void writeRequestToChannel(final HttpClientRequest request) throws DtoWriteException, IOException {
         final var body = request.body().orElse(null);
         final var headers = request.headers().unwrap();
         final var method = convert(request.method().orElseThrow(() -> new IllegalArgumentException("Expected method")));
@@ -127,7 +124,7 @@ public class NettyHttpClientConnection implements HttpClientConnection {
 
         final var remoteSocketAddress = remoteSocketAddress();
         headers.set(HOST, remoteSocketAddress.getAddress().getHostAddress() + ":" + remoteSocketAddress.getPort());
-        HttpUtil.setKeepAlive(headers, version, keepAlive);
+        HttpUtil.setKeepAlive(headers, version, !isClosing);
 
         final ByteBuf content;
         if (body == null) {
@@ -220,14 +217,11 @@ public class NettyHttpClientConnection implements HttpClientConnection {
 
         /*
          * Cancelling simply causes the response to be ignored. If not wanting
-         * the response to be received at all the client must be closed.
+         * the response to be received at all the connection must be closed.
          */
         @Override
         public void cancel(final boolean mayInterruptIfRunning) {
-            if (isDone) {
-                return;
-            }
-            setResult(Result.failure(new CancellationException()));
+            isDone = true;
         }
 
         public boolean setResult(final Result<HttpClientResponse> result) {

kalix-base/src/main/java/se/arkalix/internal/net/http/client/NettyHttpClientConnectionHandler.java
@@ -90,6 +90,9 @@ public class NettyHttpClientConnectionHandler extends NettySimpleChannelInboundH
             body.finish();
             body = null;
         }
+        if (connection.isClosing()) {
+            ctx.close();
+        }
     }
 
     private void handleResponseHead(final ChannelHandlerContext ctx, final HttpResponse response) {

kalix-base/src/main/java/se/arkalix/net/http/consumer/HttpConsumerConnection.java
@@ -42,7 +42,7 @@ public class HttpConsumerConnection {
     /**
      * @return Identity associated with the provider of the consumed service.
      * @throws NotSecureException If the provider is not running in secure
-     *                               mode.
+     *                            mode.
      */
     public SystemIdentity provider() {
         if (provider == null) {
@@ -76,14 +76,7 @@ public class HttpConsumerConnection {
      * @return {@code Future} {@code HttpConsumerResponse}.
      */
     public Future<HttpConsumerResponse> send(final HttpConsumerRequest request) {
-        Objects.requireNonNull(request, "Expected request");
-        request.setEncodingIfRequired(() -> encoding.asDtoEncoding().orElseThrow(() ->
-            new IllegalStateException("No DTO support is available for the " +
-                "encoding \"" + encoding + "\"; the request body must be " +
-                "encoded some other way")));
-        if (authorization != null) {
-            request.headers().setIfEmpty("authorization", authorization);
-        }
+        prepare(request);
         return connection.send(request.asClientRequest())
             .map(response -> new HttpConsumerResponse(response, encoding));
     }
@@ -98,8 +91,22 @@ public class HttpConsumerConnection {
      * @return {@code Future} {@code HttpConsumerResponse}.
      */
     public Future<HttpConsumerResponse> sendAndClose(final HttpConsumerRequest request) {
-        return send(request)
-            .flatMapResult(result -> close().mapResult(ignored -> result));
+        prepare(request);
+        return connection.sendAndClose(request.asClientRequest())
+            .map(response -> new HttpConsumerResponse(response, encoding));
+    }
+
+    private void prepare(final HttpConsumerRequest request) {
+        Objects.requireNonNull(request, "Expected request");
+
+        request.setEncodingIfRequired(() -> encoding.asDtoEncoding().orElseThrow(() ->
+            new IllegalStateException("No DTO support is available for the " +
+                "encoding \"" + encoding + "\"; the request body must be " +
+                "encoded some other way")));
+
+        if (authorization != null) {
+            request.headers().setIfEmpty("authorization", authorization);
+        }
     }
 
     /**

commit c57e934b44773b61433cfa6909efbf03cba221f5

Fix SSL handshake bug introduced by a5e687b

kalix-base/src/main/java/se/arkalix/internal/net/http/client/NettyHttpClientConnection.java
@@ -71,7 +71,7 @@ public class NettyHttpClientConnection implements HttpClientConnection {
     }
 
     public boolean isClosing() {
-        return isClosing;
+        return isClosing && pendingResponseQueue.size() == 0;
     }
 
     @Override

kalix-base/src/main/java/se/arkalix/internal/net/http/client/NettyHttpClientConnectionHandler.java
@@ -90,7 +90,7 @@ public class NettyHttpClientConnectionHandler extends NettySimpleChannelInboundH
             body.finish();
             body = null;
         }
-        if (connection.isClosing()) {
+        if (connection != null && connection.isClosing()) {
             ctx.close();
         }
     }