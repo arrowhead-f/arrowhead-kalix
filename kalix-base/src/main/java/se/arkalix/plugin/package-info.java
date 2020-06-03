c8ebd099bd99a2b5f08968557829c45c5c9a99fb
Emanuel Palm <emanuel.palm@ltu.se>
2020-06-03 16:57:51 +0200

    Add plugin package info

---
+++ kalix-base/src/main/java/se/arkalix/plugin/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * <h1>Arrowhead Kalix Plugin Interfaces</h1>
+ * This package contains interfaces relevant to so-called plugins, which can be
+ * thought of as providing re-usable functionality for complex service
+ * integrations.
+ * <p>
+ * Please refer to the interfaces in this package for more details, and to the
+ * {@link se.arkalix.plugin.Plugin Plugin} interface in particular.
+ */
+package se.arkalix.plugin;
