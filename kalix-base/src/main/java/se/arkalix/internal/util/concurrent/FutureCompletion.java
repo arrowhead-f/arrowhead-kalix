534f6f0321a9267426618dc6f07dc5e780c96e4b
Emanuel Palm <emanuel.palm@ltu.se>
2020-09-17 14:28:15 +0200

    Fix synchronization issue in FutureCompletion

--- kalix-base/src/main/java/se/arkalix/internal/util/concurrent/FutureCompletion.java
+++ kalix-base/src/main/java/se/arkalix/internal/util/concurrent/FutureCompletion.java
@@ -4,36 +4,46 @@ import se.arkalix.util.Result;
 import se.arkalix.util.annotation.Internal;
 import se.arkalix.util.concurrent.Future;
 
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
 
 @Internal
 public class FutureCompletion<V> implements Future<V> {
-    private final AtomicReference<Consumer<Boolean>> cancelFunction = new AtomicReference<>(null);
-    private final AtomicReference<Consumer<Result<V>>> consumer = new AtomicReference<>(null);
+    private Consumer<Boolean> cancelFunction = null;
+    private Consumer<Result<V>> consumer = null;
+    private Result<V> result = null;
 
-    public void complete(final Result<V> result) {
-        final var consumer = this.consumer.getAndSet(null);
+    public synchronized void complete(final Result<V> result) {
         if (consumer != null) {
             consumer.accept(result);
+            consumer = null;
+        }
+        else {
+            this.result = result;
         }
     }
 
-    public void setCancelFunction(final Consumer<Boolean> cancelFunction) {
-        this.cancelFunction.set(cancelFunction);
+    public synchronized void setCancelFunction(final Consumer<Boolean> cancelFunction) {
+        this.cancelFunction = cancelFunction;
     }
 
     @Override
-    public void onResult(final Consumer<Result<V>> consumer) {
-        this.consumer.set(consumer);
+    public synchronized void onResult(final Consumer<Result<V>> consumer) {
+        if (result != null) {
+            consumer.accept(result);
+            result = null;
+        }
+        else {
+            this.consumer = consumer;
+        }
     }
 
     @Override
-    public void cancel(final boolean mayInterruptIfRunning) {
-        final var cancelFunction = this.cancelFunction.getAndSet(null);
+    public synchronized void cancel(final boolean mayInterruptIfRunning) {
         if (cancelFunction != null) {
             cancelFunction.accept(mayInterruptIfRunning);
+            cancelFunction = null;
         }
-        consumer.set(null); // Best-effort attempt to prevent completion.
+        consumer = null;
+        result = null;
     }
 }
